1a464a837b1b23138d8260e74f9702621dae1db7
Daniel <36683332+dcruzf@users.noreply.github.com>
2025-11-08 17:12:24 +0000

    Deployed a3b5dea with MkDocs version: 1.6.1

---
+++ assets/js/extra.js
@@ -0,0 +1,5 @@
+// docs/js/load_pyscript.js
+const script = document.createElement("script");
+script.type = "module";
+script.src = "assets/external/pyscript.net/releases/2025.10.3/core.js";
+document.head.appendChild(script);
